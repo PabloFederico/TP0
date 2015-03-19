ca5028acc55b16c180ce507a925bc786cf069a90
PabloFederico <pablo.gabriel.federico@gmail.com>
2015-03-18 22:46:39 -0300

    agregando metodos clase piedra

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/piedra.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/piedra.java
@@ -2,6 +2,22 @@ package ar.fiuba.tecnicas.rockpaperscissors;
 
 public class piedra {
 
+public papel vs(papel papel){
 
+return papel;
+
+}
+
+public piedra vs(tijera tijera){
+
+return this;
+
+}
+
+public piedra vs(piedra piedra){
+
+return this;
+
+}
 
 }
