49ff3eeaff8eafd5cf985574155106c47843696e
PabloFederico <pablo.gabriel.federico@gmail.com>
2015-03-18 22:48:18 -0300

    agregando metodos clase tijera

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/tijera.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/tijera.java
@@ -2,6 +2,23 @@ package ar.fiuba.tecnicas.rockpaperscissors;
 
 public class tijera {
 
+public tijera vs(papel papel){
+
+return this;
+
+}
+
+public tijera vs(tijera tijera){
+
+return this;
+
+}
+
+public piedra vs(piedra piedra){
+
+return piedra;
+
+}
 
 
 }
