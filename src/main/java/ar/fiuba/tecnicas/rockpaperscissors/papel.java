60841772dbc0106be25534459e341726a8e6c904
PabloFederico <pablo.gabriel.federico@gmail.com>
2015-03-18 22:38:24 -0300

    Agregando modelo clases

---
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/papel.java
@@ -0,0 +1,29 @@
+package ar.fiuba.tecnicas.rockpaperscissors;
+
+public class papel {
+
+
+public papel vs(papel papel){
+
+return this;
+
+}
+
+public tijera vs(tijera tijera){
+
+return tijera;
+
+}
+
+public papel vs(piedra piedra){
+
+return this;
+
+}
+
+
+
+
+
+
+}
---
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/tijera.java
@@ -0,0 +1,7 @@
+package ar.fiuba.tecnicas.rockpaperscissors;
+
+public class tijera {
+
+
+
+}
